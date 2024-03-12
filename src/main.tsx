947c125d0f9f6cd29dec1822cc3ca59f457acd81
Boon <boon@rts.com>
2024-03-12 15:02:41 +0300

    feat: deploy to VK Mini App Platform

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import { AdaptivityProvider, AppRoot, ConfigProvider } from "@vkontakte/vkui";
-
+import bridge from "@vkontakte/vk-bridge";
+bridge.send("VKWebAppInit");
 createRoot(document.getElementById("root")!).render(
   <ConfigProvider>
     <AdaptivityProvider>
